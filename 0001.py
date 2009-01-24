0216ce7e77f07d08dfa6f5d812f555b26a28e129
Sebastian Nowicki <sebnow@gmail.com>
2009-01-24 16:03:22 +0900

    Solve the first problem!

---
+++ 0001.py
@@ -0,0 +1,18 @@
+def solve(min, max):
+	"""
+	If we list all the natural numbers below 10 that are multiples of 3 or 5, we
+	get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+	>>> solve(0, 10)
+	23
+	>>> solve(0, 1000)
+	233168
+	"""
+	sum = 0
+	for i in range(min, max):
+		if i % 3 == 0 or i % 5 == 0:
+			sum = sum + i
+	return sum
+ 
+if __name__ == "__main__":
+	print "Answer:", solve(0, 1000)
